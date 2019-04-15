a5756d31763553ac5742f12169001ae6f27dfa77
VKorn <vald9@mail.ru>
2019-04-16 01:39:44 +0300

    add logic to important button, fixed important color

--- src/components/TodoListItem/TodoListItem.js
+++ src/components/TodoListItem/TodoListItem.js
@@ -7,9 +7,11 @@ export default class TodoListItem extends Component {
 
     this.state = {
       done: false,
+      important: false,
     };
 
     this.onLabelClick = this.onLabelClick.bind(this);
+    this.onImportantClick = this.onImportantClick.bind(this);
   }
 
   onLabelClick() {
@@ -18,33 +20,43 @@ export default class TodoListItem extends Component {
     });
   }
 
-  render() {
-    const { label, important = false } = this.props;
-    const { done } = this.state;
+  onImportantClick() {
+    this.setState({
+      important: !this.state.important,
+    });
+  }
 
-    let isItemDone =  ' todo-list-item';
+  render() {
+    const { label } = this.props;
+    const { done, important } = this.state;
+
+    let itemClassName = 'todo-list-item';
     if (done) {
-      isItemDone += ' done';
+      itemClassName += ' done';
+    }
+    if (important) {
+      itemClassName += ' important';
     }
 
-    const style = {
-      color: important ? 'steelBlue' : 'black',
-      fontWeight: important ? 'bold' : 'normal',
-    };
-
     return (
-      <span className={isItemDone}>
+      <span className={itemClassName}>
         <span
           className="todo-list-item__label"
-          style={style}
           onClick={this.onLabelClick}
         >{label}
         </span>
           <div className="todo-list-buttons float-right">
-            <button className="btn btn-outline-success btn-sm" type="button">
+            <button className="btn btn-outline-success btn-sm"
+                    type="button"
+                    title="Delete"
+            >
               <i className="fa fa-trash-o"/>
             </button>
-            <button className="btn btn-outline-danger btn-sm" type="button">
+            <button className="btn btn-outline-danger btn-sm"
+                    type="button"
+                    title="Important"
+                    onClick={this.onImportantClick}
+            >
               <i className="fa fa-exclamation"/>
             </button>
           </div>
